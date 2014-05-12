07b5609aba3b0ccbb78edd3973ce38db1e9328b1
William Villanueva <wvillanu89@gmail.com>
2014-05-11 21:38:48 -0400

    cleaned up the code to be more efficient and readable.

--- main.js
+++ main.js
@@ -1,100 +1,96 @@
-
-
-      
-
-
-      $(document).ready(function(){
-        var userstream=streams.home;
-        var $tweets = $('.tweets');
-        var data ={'shawndrost':'Just Shawnin\' it','sharksforcheap':'Just Sharkin\' it','mracus':'Just Mracin\' it','douglascalhoun':'Just Dougin\' it'};
-        var showusertweets = function(userOrMessage,user){
-          $tweets.children('div').remove();
-          var index = userOrMessage.length - 1;
-          var today=new Date();
-        var day;
-        while(index >= 0){
-          var tweet = userOrMessage[index];
-          var time = tweet.created_at;
-          var diff= today-time;
-          if (diff/1000/60<59) {
-            day=Math.ceil(diff/1000/60)+'m';
-          } else if ((diff/1000/60/60) < 24){
-            day=Math.floor(diff/1000/60/60)+'h';
-          } else {
-            day=time.toString().slice(4,10)
-          }
-          var $tweet = $('<div><h1>'+tweet.user+'</h1><h2>@'+tweet.user+' - '+day+'</h2><p>'+tweet.message+'</p</div>');
+$(document).ready(function(){
+  //initial definitions
+  var userstream=streams.home;
+  var $tweets = $('.tweets');
+  var data ={'shawndrost':'Just Shawnin\' it','sharksforcheap':'Just Sharkin\' it','mracus':'Just Mracin\' it','douglascalhoun':'Just Dougin\' it'};
   
-          $tweet.appendTo($tweets);
-          index -= 1;
+  //functions
+  var showusertweets = function(userOrMessage,user){
+    $tweets.children('div').remove();
+    var index = userOrMessage.length - 1;
+    var today=new Date();
+    var day;
+    while(index >= 0){
+      var tweet = userOrMessage[index];
+      var time = tweet.created_at;
+      var diff= today-time;
+      if (diff/1000/60<59) {
+        day=Math.ceil(diff/1000/60)+'m';
+      } else if ((diff/1000/60/60) < 24){
+        day=Math.floor(diff/1000/60/60)+'h';
+      } else {
+        day=time.toString().slice(4,10)
+      }
+        var $tweet = $('<div><h1>'+tweet.user+'</h1><h2>@'+tweet.user+' - '+day+'</h2><p>'+tweet.message+'</p</div>');
+        $tweet.appendTo($tweets);
+        index -= 1;
+    };
+    $('.tweets div').click(displayUser)
+    $('#user').click(function (evt) {evt.preventDefault(); showusertweets(userstream);});
+    $('#crunch img').click(function (evt) {evt.preventDefault(); showusertweets(userstream);});
+    $(window).scrollTop(0)
+    if (typeof user === 'string') {
+      userstream=streams.users[user];
+    }
+  };
 
-        };
-        $('.tweets div').click(displayUser)
-        $('#user').click(function (evt) {evt.preventDefault(); showusertweets(userstream);});
-        $('#crunch img').click(function (evt) {evt.preventDefault(); showusertweets(userstream);});
 
-        $(window).scrollTop(0)
-        if (typeof user === 'string') {
-          userstream=streams.users[user];
-        }
+  var homestream = function (evt) {
+    $('#crunch').hide();
+    $('#header').css('height','230px');
+    $('#center').css('top','45px');
+    $('#texttweet').show();
+    userstream=streams.home;
+    showusertweets(userstream);
+    evt.preventDefault();
+  };
 
-      };
 
+  var displayUser = function() {
+    $('#crunch').html('');
+    var user=($(this).find('h1').text());
+    if (user==='Me') {
+      showusertweets(userstream);
+    } else {
+      $('#crunch').append('<img src='+user+'.jpg id="portrait">')
+      $('#crunch').append('<div id=user>. . . '+data[user]+'</div>')
+      $('#crunch').append('<div id=user>@'+user+'</div>')
+      $('#header').css('height','380px');
+      $('#center').css('top','30px');
+      $('#texttweet').hide();
+      $('#crunch').show();
+      userstream=streams.users[user];
+      showusertweets(userstream,user);
+    }
+  };
 
-         $('#texttweet').focus(function (){
-          $('#texttweet').attr('rows',3);
-          $('#box').show();});
-         $('#texttweet').blur(function(){
-          setTimeout(function(){$('#texttweet').attr('rows',1);
-            $('#box').hide(); $('#texttweet').val('')},200);});
+//event Handlers
+    
+  $('#texttweet').focus(function (){
+    $('#texttweet').attr('rows',3);
+    $('#box').show();});
+    $('#texttweet').blur(function(){
+    setTimeout(function(){$('#texttweet').attr('rows',1);
+    $('#box').hide(); $('#texttweet').val('')},300);});
           
-        
-        var homestream = function (evt) {
-          $('#crunch').hide();
-          $('#header').css('height','230px');
-          $('#center').css('top','45px');
-          $('#texttweet').show();
-          userstream=streams.home;
-          showusertweets(userstream);
-          evt.preventDefault();
-        }
 
+  $('#center').click(function (evt) {
+    evt.preventDefault(); 
+    showusertweets(userstream);});
 
-        $('#center').click(function (evt) {evt.preventDefault(); showusertweets(userstream);});
-        $('.tweets div').click(displayUser)
-        $('#homeicon').click(homestream);
-        $('#twitcontain').click(homestream);
+  $('.tweets div').click(displayUser)
+  $('#homeicon').click(homestream);
+  $('#twitcontain').click(homestream);
 
+  $('#send').click(function(){
+    var message=$('#texttweet').val();
+    var time=new Date();
+    var user='Me';
+    newTweet={created_at:time,message:message,user:user};
+    streams.home.push(newTweet);
+    showusertweets(userstream);
+    });
 
-        function displayUser() {
-          $('#crunch').html('');
-          var user=($(this).find('h1').text());
-          console.log(user);
-          if (user==='Me') {
-            showusertweets(userstream);
-          } else {
-          $('#crunch').append('<img src='+user+'.jpg id="portrait">')
-          $('#crunch').append('<div id=user>. . . '+data[user]+'</div>')
-          $('#crunch').append('<div id=user>@'+user+'</div>')
-          $('#header').css('height','380px');
-          $('#center').css('top','30px');
-          $('#texttweet').hide();
-          $('#crunch').show();
-          userstream=streams.users[user];
-          showusertweets(userstream,user);
-          }
-
-        }
-
-        $('#send').click(function(){
-         var message=$('#texttweet').val();
-         var time=new Date();
-         var user='Me';
-         newTweet={created_at:time,message:message,user:user};
-         streams.home.push(newTweet);
-         showusertweets(userstream);
-        });
-
-      });
+});
 
     
