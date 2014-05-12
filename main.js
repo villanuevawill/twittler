47951859a88ab857932a3c83e2a22f6f0e960504
William Villanueva <wvillanu89@gmail.com>
2014-05-11 20:52:51 -0400

    Fixed a bug that would keep the user from loading the homepage.  Also added a UX enhancement.

--- main.js
+++ main.js
@@ -46,38 +46,8 @@
           $('#box').show();});
          $('#texttweet').blur(function(){
           setTimeout(function(){$('#texttweet').attr('rows',1);
-            $('#box').hide(); $('#textweet').val('')},200);});
+            $('#box').hide(); $('#texttweet').val('')},200);});
           
-         
-
-
-
-        
-        /*var showtweets = function(evt){
-        $tweets.children('div').remove();
-        var index = streams.home.length - 1;
-        var today=new Date();
-        var day;
-        while(index >= 0){
-          var tweet = streams.home[index];
-          var time = tweet.created_at;
-          var diff= today-time;
-          if (diff/1000/60<59) {
-            day=Math.ceil(diff/1000/60)+'m';
-          } else if ((diff/1000/60/60) < 24){
-            day=Math.floor(diff/1000/60/60)+'h';
-          } else {
-            day=time.toString().slice(4,10)
-          }
-          var $tweet = $('<div><h1>'+tweet.user+'</h1><h2>@'+tweet.user+' - '+day+'</h2><p>'+tweet.message+'</p</div>');
-  
-          $tweet.appendTo($tweets);
-          index -= 1;
-
-        }
-        evt.preventDefault();
-        $('.tweets div').click(displayUser)
-      };*/
         
         var homestream = function (evt) {
           $('#crunch').hide();
@@ -99,6 +69,10 @@
         function displayUser() {
           $('#crunch').html('');
           var user=($(this).find('h1').text());
+          console.log(user);
+          if (user==='Me') {
+            showusertweets(userstream);
+          } else {
           $('#crunch').append('<img src='+user+'.jpg id="portrait">')
           $('#crunch').append('<div id=user>. . . '+data[user]+'</div>')
           $('#crunch').append('<div id=user>@'+user+'</div>')
@@ -108,6 +82,7 @@
           $('#crunch').show();
           userstream=streams.users[user];
           showusertweets(userstream,user);
+          }
 
         }
 
